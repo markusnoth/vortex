7487cc422cee436e0a6b033d499ace327544c0ab
Markus Noth <markus.noth@swisstxt.ch>
2017-08-08 16:27:28 +0200

    only run server when vortex connection ready

--- server.js
+++ server.js
@@ -7,29 +7,31 @@ const PORT = process.env.PORT || 8080
 
 const client = new VortexClient(config.vortex)
 client.connect()
-    .then(response => console.log(response))
-    .catch(error => console.error(error))
+    .then(response => {
+        console.log(response)
+        const app = express()
 
-const app = express()
+        // SETUP URL-REWRITING FOR IISNODE
+        if (process.env.VIRTUAL_DIR_PATH) {
+            app.use((req, res, next) => {
+                req.url = req.url.replace(new RegExp('^' + process.env.VIRTUAL_DIR_PATH), '')
+                next()
+            })
+        }
 
-// SETUP URL-REWRITING FOR IISNODE
-if (process.env.VIRTUAL_DIR_PATH) {
-    app.use((req, res, next) => {
-        req.url = req.url.replace(new RegExp('^' + process.env.VIRTUAL_DIR_PATH), '')
-        next()
-    })
-}
-
-app.get('/page/:mag/:set?/:page?', (req, res, next) => {
-    const { mag, set, page } = req.params
-    client.getPage(mag, set, page)
-        .then(response => {
-            res.setHeader('Content-Type', 'application/ep1')
-            res.send(Buffer.from(response))
+        app.get('/page/:mag/:set?/:page?', (req, res, next) => {
+            const { mag, set, page } = req.params
+            client.getPage(mag, set, page)
+                .then(response => {
+                    res.setHeader('Content-Disposition', `attachment; filename=${mag}_${set || 0}_${page || 0}.ep1`);
+                    res.setHeader('Content-Type', 'application/ep1')
+                    res.send(Buffer.from(response))
+                })
+                .catch(error => res.status(500).send(error.message || error))
         })
-        .catch(error => res.status(500).send(error.message || error))
-})
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
-})
+        app.listen(PORT, () => {
+            console.log(`Server listening on port ${PORT}`)
+        })
+    })
+    .catch(error => console.error(error))
